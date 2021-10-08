5f3fd24da4ca1bb6f905db21924ea996b3398d55
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-08 16:51:25 -0600

    Bump version.

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ import os
 class Application:
 	def __init__(self):
 		self.name = "Quinread"
-		self.version = "0.26"
+		self.version = "0.27"
 		self.running = False
 		self.config = None
 		self.load_config()
