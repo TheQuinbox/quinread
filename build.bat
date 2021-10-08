2926003d676bdc1d1ae207b55a08023668c2154e
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-08 11:14:50 -0600

    Bump version.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-nuitka --windows-company-name=Accessware --windows-product-name=Quinread --windows-file-version=0.25 --windows-product-version=0.25 --windows-file-description=Quinread --standalone --python-flag=no_site --windows-disable-console --remove-output quinread.pyw
+nuitka --windows-company-name=Accessware --windows-product-name=Quinread --windows-file-version=0.26 --windows-product-version=0.26 --windows-file-description=Quinread --standalone --python-flag=no_site --windows-disable-console --remove-output quinread.pyw
 pause > nul
