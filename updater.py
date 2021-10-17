66bafd1721dd332c8f8892a3e51929363f76eb01
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-17 10:44:39 -0600

    Make the update checker work better.

--- updater.py
+++ updater.py
@@ -1,17 +1,16 @@
 import json
 import requests
-import threading
+import webbrowser
 
 def update_check(app, silent=True):
 	try:
 		latest = json.loads(requests.get("https://api.github.com/repos/TheQuinbox/quinread/releases/latest",{"accept":"application/vnd.github.v3+json"}).content.decode())
 		if app.version < latest["tag_name"]:
-			ud = question("Update available: " + latest["tag_name"],"There is an update available. Your version: " + app.version + ". Latest version: " + latest["tag_name"] + ". Description: " + latest["body"] + "\r\nDo you want to open the direct download link?")
-			if ud == 1:
-				for i in latest["assets"]:
-					if "quinread.zip" in i["name"].lower():
-						threading.Thread(target=download_update, args=[i["browser_download_url"],], daemon=True).start()
-						return
+			dlg=wx.MessageDialog(app.main_frame, "There is an update available. Your version: " + app.version + ". Latest version: " + latest["tag_name"] + ". Description: " + latest["body"] + "\nDo you want to open the direct download link?", "Update available: " + latest["tag_name"], wx.YES_NO | wx.ICON_QUESTION)
+			result=dlg.ShowModal()
+			dlg.Destroy()
+			if result == wx.ID_YES:
+				webbrowser.open(i["browser_download_url"])
 		else:
 			if not silent:
 				alert("No updates available! The latest version of the program is " + latest["tag_name"],"No update available")
