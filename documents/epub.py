d8c7511b5eb70e0be898fa2be642ce1496831c67
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-22 12:56:10 -0600

    Switch to fitz for epub parsing.

--- documents/epub.py
+++ documents/epub.py
@@ -1,21 +1,18 @@
 from .base import BaseDocument
-from ebooklib import epub, ITEM_DOCUMENT
-import html_parser
+import fitz
+import re
 
 class EpubDocument(BaseDocument):
 	def __init__(self, path):
 		self.path = path
 
 	def read(self):
-		self.document = epub.read_epub(self.path)
-		result = ""
-		for id, _ in self.document.spine:
-			item = self.document.get_item_with_id(id)
-			if item is None:
-				continue
-			result += item.content.decode()
-		result = html_parser.html_to_text(result)
-		return result
+		final = ""
+		self.document = fitz.open(self.path)
+		for page in self.document:
+			text = page.get_text().encode("utf8")
+			final += re.sub(" +\n", "\n", text.decode())
+		return re.sub(r"\n\s*\n", "\n\n", final)
 
 	def close(self):
 		super().close()
