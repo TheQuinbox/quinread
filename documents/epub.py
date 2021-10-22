c04b6a46ca8146a8c5fe7e07a76c6be58b3f878c
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-22 13:02:13 -0600

    Revert "Switch to fitz for epub parsing."
    This reverts commit d8c7511b5eb70e0be898fa2be642ce1496831c67.
    
    It was way too slow, so let's not do that

--- documents/epub.py
+++ documents/epub.py
@@ -1,18 +1,21 @@
 from .base import BaseDocument
-import fitz
-import re
+from ebooklib import epub, ITEM_DOCUMENT
+import html_parser
 
 class EpubDocument(BaseDocument):
 	def __init__(self, path):
 		self.path = path
 
 	def read(self):
-		final = ""
-		self.document = fitz.open(self.path)
-		for page in self.document:
-			text = page.get_text().encode("utf8")
-			final += re.sub(" +\n", "\n", text.decode())
-		return re.sub(r"\n\s*\n", "\n\n", final)
+		self.document = epub.read_epub(self.path)
+		result = ""
+		for id, _ in self.document.spine:
+			item = self.document.get_item_with_id(id)
+			if item is None:
+				continue
+			result += item.content.decode()
+		result = html_parser.html_to_text(result)
+		return result
 
 	def close(self):
 		super().close()
