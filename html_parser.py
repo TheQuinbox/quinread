78973152c06ad0f77d9862979bc57fa9b2da6c48
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-27 17:23:32 -0600

    Remove span.

--- html_parser.py
+++ html_parser.py
@@ -9,7 +9,7 @@ class HtmlToText(HTMLParser):
 		self.hide_output = False
 
 	def handle_starttag(self, tag, attrs):
-		if tag in ("b", "i", "span"):
+		if tag in ("b", "i"):
 			return
 		elif tag in ("p", "br", "div", "h1", "h2", "h3", "h4", "h5", "h6", "blockquote", "li") and not self.hide_output:
 			self._buf.append("\n")
