0a1a9ff97e9dbe04b4b619b0dd48c2df9ea64fac
Quin Marilyn <quin.marilyn05@gmail.com>
2021-11-15 12:37:22 -0700

    Slight formatting in html_parser.

--- html_parser.py
+++ html_parser.py
@@ -5,23 +5,22 @@ import re
 class HtmlToText(HTMLParser):
 	def __init__(self):
 		HTMLParser.__init__(self)
-		self._buf = []
+		self.buf = []
 		self.hide_output = False
 		self.in_paragraph = False
 	
 	def handle_starttag(self, tag, attrs):
-		if tag in ("b", "i"):
-			return
+		if tag in ("b", "i"): return
 		if tag in ("p", "div"):
 			self.in_paragraph = True
 		if tag in ("p", "br", "div", "h1", "h2", "h3", "h4", "h5", "h6", "blockquote", "li") and not self.hide_output:
-			self._buf.append("\n")
+			self.buf.append("\n")
 		if tag in ("script", "style", "title"):
 			self.hide_output = True
 	
 	def handle_startendtag(self, tag, attrs):
 		if tag == "br":
-			self._buf.append("\n")
+			self.buf.append("\n")
 	
 	def handle_endtag(self, tag):
 		if tag in ("script", "style", "title"):
@@ -35,20 +34,20 @@ class HtmlToText(HTMLParser):
 				text = text.replace("\r\n", " ")
 				text = text.replace("\n", " ")
 			text = re.sub(r"\s*\n\s*", "\n", text)
-			self._buf.append(text)
+			self.buf.append(text)
 	
 	def handle_entityref(self, name):
 		if name in name2codepoint and not self.hide_output:
 			c = unichr(name2codepoint[name])
-			self._buf.append(c)
+			self.buf.append(c)
 	
 	def handle_charref(self, name):
 		if not self.hide_output:
 			n = int(name[1:], 16) if name.startswith("x") else int(name)
-			self._buf.append(unichr(n))
+			self.buf.append(unichr(n))
 	
 	def get_text(self):
-		return re.sub(r" +", " ", "".join(self._buf))
+		return re.sub(r" +", " ", "".join(self.buf))
 
 def html_to_text(html):
 	parser = HtmlToText()
